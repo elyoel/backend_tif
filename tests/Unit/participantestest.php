04432be9c4d0ec56769fac558e4de437e570af06
alexrs666 <losrfrosaflores@gmail.com>
2024-10-30 17:40:09 -0300

    actualiza la hermana de yoel

--- tests/Unit/participantestest.php
+++ tests/Unit/participantestest.php
@@ -3,18 +3,19 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use App\Models\Partcipantes;
+use App\Models\Participantes;
+
 class participantestest extends TestCase
 {
     public function test_particiapntestest_nombre() {
-        $partiejemplo=new participantes();
+        $partiejemplo = new Participantes();
         $partiejemplo-> setNombre("yoel");
 
         $this->assertEquals("YOEL", $partiejemplo->getNombre());
     }
     
     public function test_particiapntestest_apellido() {
-        $partiejemplo=new participantes();
+        $partiejemplo = new Participantes();
         $partiejemplo-> setApellido("calderon");
 
         $this->assertEquals("CALDERON", $partiejemplo->getApellido());
